eda84957f52def81b9fb3d8d5e0627a3472edac5
91neb <benbueoficial@gmail.com>
2024-06-27 13:12:54 -0400

    yeah

---
+++ src/main/java/net/ben19/emmbdm/ElModMasBakanDelMundoClient.java
@@ -0,0 +1,10 @@
+package net.ben19.emmbdm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElModMasBakanDelMundoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ben19/emmbdm/ElModMasBakanDelMundoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ben19.emmbdm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElModMasBakanDelMundoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
